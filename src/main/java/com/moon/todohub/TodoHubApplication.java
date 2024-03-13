f9cfeedb0ec66ed102d0940f660c95b775ed2ec7
MOON <moonkihyun95@gmail.com>
2024-03-14 06:12:18 +0900

    git init

---
+++ src/main/java/com/moon/todohub/TodoHubApplication.java
@@ -0,0 +1,13 @@
+package com.moon.todohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoHubApplication.class, args);
+    }
+
+}
